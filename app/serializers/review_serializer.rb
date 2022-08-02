5f1f17415a3b99fd25a81ed48882efdabf3b7732
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-08-02 17:17:21 -0400

    serializers/routes/products table update

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,14 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :comment, :rating, :user_id, :product_id
+
+
+  #   def average_rating
+  #     self.object.rating
+  #   end
+
+
+  # def review_sum
+  #   self.object.rating
+  # end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_145910) do
+ActiveRecord::Schema.define(version: 2022_08_02_210549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.string "price"
     t.string "img_url"
     t.string "product_img"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "avg_rating"
   end
 
   create_table "reviews", force: :cascade do |t|
