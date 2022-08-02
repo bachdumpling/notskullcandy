a602c620ba835f110bc95db153c95ba2876ffb87
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-08-02 17:56:06 -0400

    working

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,9 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-    attributes :id, :name, :price, :img_url, :product_img, :avg_rating
-
-    
-=======
-    attributes :id, :name, :price, :img_url, :product_img, :description
->>>>>>> refs/remotes/origin/main
+    attributes :id, :name, :price, :img_url, :product_img, :description, :avg_rating
 end
